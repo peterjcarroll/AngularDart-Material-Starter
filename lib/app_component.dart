af2a90a9482b80cafb3794a3fcc768bf93af0cd3
Peter Carroll <peter@eringal.com>
2016-10-30 14:45:36 -0500

    Initial commit

---
+++ lib/app_component.dart
@@ -0,0 +1,13 @@
+// Copyright (c) 2016, PeterCarroll. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+import 'package:angular2/core.dart';
+import 'package:angular2_components/angular2_components.dart';
+
+@Component(
+    selector: 'my-app',
+    styleUrls: const ['app_component.css'],
+    templateUrl: 'app_component.html',
+    directives: const [materialDirectives],
+    providers: const [materialProviders])
+class AppComponent {}
